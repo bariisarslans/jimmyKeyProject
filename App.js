e178c955ed48a810931f07f89f1dbc08efed3f69
bariisarslans <bariisarslans@hotmail.com>
2021-03-10 18:12:56 +0300

    payload handle

--- App.js
+++ App.js
@@ -1,72 +1,93 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
+  Button,
   View,
-  Text,
-  StatusBar,
+  ActivityIndicator,
+  Platform
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { addEventListener, removeEventListener, requestPermissions, EuroMessageApi, VisilabsApi } from 'react-native-related-digital'
 
-const App: () => React$Node = () => {
+const App = () => {
+  const [loading, setLoading] = useState(false)
+
+  const appAlias = Platform.OS === "ios" ? "jimmykeyios" : "jimmykeyandroid";
+  const siteId = "356467332F6533766975593D";
+  const organizationId = "676D325830564761676D453D";
+  const dataSource = "visistore";
+
+
+  const euroMessageApi = new EuroMessageApi(appAlias)
+  const visilabsApi = new VisilabsApi(appAlias, siteId, organizationId, dataSource)
+
+  useEffect(() => {
+    addExtra()
+    addListeners()
+    requestPermissions()
+
+    return () => removeListeners()
+  }, [])
+
+  const addListeners = () => {
+
+    addEventListener('register', async (token) => {
+      console.log("token",token);
+      const subscribeResult = await euroMessageApi.subscribe(token)
+
+      console.log("subscribeResult",subscribeResult);
+
+    }, (notificationPayload) => {
+      console.log('notification payload', notificationPayload)
+    }, euroMessageApi, visilabsApi)
+
+    addEventListener('registrationError', async (registrationError) => {
+      console.log('registrationError is ', registrationError)
+    }, euroMessageApi)
+  }
+
+  const getCustomTime = () => {
+    var d = new Date();
+    var date_format_str = d.getFullYear().toString() + "-" + ((d.getMonth() + 1).toString().length == 2 ? (d.getMonth() + 1).toString() : "0" + (d.getMonth() + 1).toString()) + "-" + (d.getDate().toString().length == 2 ? d.getDate().toString() : "0" + d.getDate().toString()) + " " + (d.getHours().toString().length == 2 ? d.getHours().toString() : "0" + d.getHours().toString()) + ":" + ((d.getMinutes()).toString().length == 2 ? (d.getMinutes()).toString() : "0" + (d.getMinutes()).toString()) + ":" + (d.getSeconds()).toString();
+    return date_format_str
+  }
+
+  const addExtra = async () => { 
+    await euroMessageApi.setUserProperty("email", "baris.arslan@euromsg.com");
+    await euroMessageApi.setUserProperty("keyID", "baris.arslan@euromsg.com");
+    await euroMessageApi.setUserProperty("pushPermit", "Y");
+    await euroMessageApi.setUserProperty('ConsentTime', getCustomTime());
+    await euroMessageApi.setUserProperty('RecipientType', "BIREYSEL");
+    await euroMessageApi.setUserProperty('ConsentSource', "HS_MOBIL");
+  }
+
+  const removeListeners = () => {
+    removeEventListener('register')
+    removeEventListener('registrationError')
+  }
+
   return (
     <>
-      <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+        {
+          loading ?
+          <ActivityIndicator 
+            size='large'
+            animating={loading} /> :
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            
+            <Button 
+              title='REQUEST PERMISSONS'
+              onPress={() => {
+                requestPermissions()
+              }} 
+            />
+          </ScrollView>
+        }
       </SafeAreaView>
     </>
   );
@@ -74,41 +95,12 @@ const App: () => React$Node = () => {
 
 const styles = StyleSheet.create({
   scrollView: {
-    backgroundColor: Colors.lighter,
+    backgroundColor: '#FFF',
+    padding: 20
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  divider: {
+    height: 20
+  }
 });
 
 export default App;
--- android/app/src/main/java/com/jimmykeyproject/MainApplication.java
+++ android/app/src/main/java/com/jimmykeyproject/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.visilabs.Visilabs;
+import euromsg.com.euromobileandroid.EuroMobileManager;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -45,8 +47,28 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+      initEuroMessage();
   }
 
+    private void initEuroMessage() {
+        String appAlias = "demo-alias";
+        String huaweiAppAlias = "demo-alias-huawei";
+        String organizationId = "676D325830564761676D453D";
+        String siteId = "356467332F6533766975593D";
+        String datasource = "visistore";
+        String channel = "Android";
+        String segmentUrl = "http://lgr.visilabs.net";
+        String realtimeUrl = "http://rt.visilabs.net";
+        String targetUrl = "http://s.visilabs.net/json";
+        String actionUrl = "http://s.visilabs.net/actjson";
+        String geofenceUrl = "http://s.visilabs.net/geojson";
+
+        Visilabs.CreateAPI(organizationId, siteId, segmentUrl,
+                datasource, realtimeUrl, channel, this, targetUrl, actionUrl, 30000, geofenceUrl, true);
+
+        EuroMobileManager euroMobileManager = EuroMobileManager.init(appAlias, huaweiAppAlias, this);
+        euroMobileManager.setPushIntent("com.demo.MainActivity", this);
+    }
   /**
    * Loads Flipper in React Native templates. Call this in the onCreate method with something like
    * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
